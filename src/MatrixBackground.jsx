bd014b5141393bf8b421ad4402bb936d3b5fcc3f
rizz <ganzz808@gmail.com>
2025-11-02 06:48:10 +0700

    asd

--- src/MatrixBackground.jsx
+++ src/MatrixBackground.jsx
@@ -25,7 +25,7 @@ const MatrixBackground = () => {
       ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
       ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-      ctx.fillStyle = '#00FF00'; 
+      ctx.fillStyle = '#00ff9d'; 
       ctx.font = `${fontSize}px 'VT323', monospace`; 
 
       for (let i = 0; i < drops.length; i++) {
