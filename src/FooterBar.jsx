ff5438e79761c9c04a42ac36b84c14777adae211
rizz <ganzz808@gmail.com>
2025-11-02 08:35:03 +0700

    asd

--- src/FooterBar.jsx
+++ src/FooterBar.jsx
@@ -5,16 +5,27 @@ import React from 'react';
 function FooterBar({ onLinkClick }) {
   return (
     <div className="footer-bar">
+      
+      {/* Kolom Kiri */}
       <div className="footer-left">
-        <span>Pumpflip 2025</span>
+        <span>© Pumpflip 2025</span>
       </div>
-      <div className="footer-right">
-        {/* Ganti <a> dengan <span> dan tambahkan onClick */}
+      
+      {/* Kolom Tengah (BARU) */}
+      <div className="footer-center">
         <span className="footer-link" onClick={() => onLinkClick('privacy')}>Privacy Policy</span>
+        <span className="footer-separator">|</span>
         <span className="footer-link" onClick={() => onLinkClick('tos')}>Terms of Service</span>
+        <span className="footer-separator">|</span>
         <span className="footer-link" onClick={() => onLinkClick('fees')}>Fees</span>
+        <span className="footer-separator">|</span>
         <span className="footer-link" onClick={() => onLinkClick('updates')}>Tech Updates</span>
       </div>
+
+      {/* Kolom Kanan (Kosong, untuk menyeimbangkan layout) */}
+      <div className="footer-right">
+      </div>
+
     </div>
   );
 }
